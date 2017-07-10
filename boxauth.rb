ad5a613525c0581487e49b89da8f801805ef77b6
Band-Aid <da-ishi@daichibox.com>
2017-07-10 13:34:34 +0900

    Update boxauth.rb

--- boxauth.rb
+++ boxauth.rb
@@ -8,7 +8,7 @@ get '/oauth' do
 end
 
 get '/authenticate' do
-   redirect to("https://account.box.com/api/oauth2/authorize?response_type=code&client_id=uxxx&redirect_uri=http://localhost:4567/oauth&state=xxx")
+   redirect to("https://account.box.com/api/oauth2/authorize?response_type=code&client_id=uxxx&redirect_uri={your redirect url}oauth&state=xxx")
 end
 
 def gettoken(code)
