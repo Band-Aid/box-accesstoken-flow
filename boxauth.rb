77b86abba68e397011ebe986e2fc40cad1063b50
Band-Aid <da-ishi@daichibox.com>
2017-08-10 11:08:32 +0900

    Update boxauth.rb

--- boxauth.rb
+++ boxauth.rb
@@ -2,17 +2,18 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-get '/oauth' do
-    code = params[:code]
-    gettoken(code)
-end
-
 get '/authenticate' do
     #set the redirect URL to get /oauth
    redirect to("https://account.box.com/api/oauth2/authorize?response_type=code&client_id=uxxx&redirect_uri={your redirect url + /oauth}&state=xxx")
 end
 
-def gettoken(code)
+get '/oauth' do #get token using auth code
+    
+    code = params[:code]
+    gettoken(code)
+end
+
+def gettoken(code) #exchange access token pair with auth-code
     begin
     res = RestClient.post('https://api.box.com/oauth2/token', {grant_type: 'authorization_code', code: code, client_id: 'xxx', client_secret: 'xxx'})
     parsed = JSON.parse(res.body)    
