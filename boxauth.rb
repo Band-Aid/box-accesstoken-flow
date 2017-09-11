2262267af60c38b7bc94997fb5704c7673e2c5d3
Band-Aid <da-ishi@daichibox.com>
2017-09-11 15:14:58 +0900

    Update boxauth.rb

--- boxauth.rb
+++ boxauth.rb
@@ -4,7 +4,7 @@ require 'json'
 
 get '/authenticate' do
     #set the redirect URL to get /oauth
-   redirect to("https://account.box.com/api/oauth2/authorize?response_type=code&client_id=uxxx&redirect_uri={your redirect url + /oauth}&state=xxx")
+   redirect to("https://account.box.com/api/oauth2/authorize?response_type=code&client_id={your client ID}&redirect_uri={your redirect url + /oauth}&state={your client secret}")
 end
 
 get '/oauth' do #get token using auth code
@@ -15,7 +15,7 @@ end
 
 def gettoken(code) #exchange access token pair with auth-code
     begin
-    res = RestClient.post('https://api.box.com/oauth2/token', {grant_type: 'authorization_code', code: code, client_id: 'xxx', client_secret: 'xxx'})
+    res = RestClient.post('https://api.box.com/oauth2/token', {grant_type: 'authorization_code', code: code, client_id: '{your client ID}', client_secret: '{your client secret}'})
     parsed = JSON.parse(res.body)    
 
     
